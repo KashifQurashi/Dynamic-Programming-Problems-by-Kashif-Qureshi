c0f5b088b7ba247c58d76be77728675da66270f3
Kashif Qurashi <kashifqureshi481@gmail.com>
2022-11-10 13:40:16 +0500

    Add files via upload

---
+++ problem 16 - Ninja's Trip(Minimum cost of Ticket)(Space Optimization).cpp
@@ -0,0 +1,27 @@
+int minimumCoins(int n, vector<int> days, vector<int> cost)
+{
+    int ans = 0;
+    queue<pair<int, int>> month;
+    queue<pair<int, int>> week;
+    
+    
+    
+    for(int day: days) {
+        
+        // Step 1: remove expired days
+        while(!month.empty() && month.front().first + 30 <= day)
+            month.pop();
+        
+        while(!week.empty() && week.front().first + 7 <= day)
+            week.pop();
+        
+        // Step 2: Add cost for current day
+        week.push(make_pair(day, ans+cost[1]));
+        month.push(make_pair(day, ans+cost[2]));
+        
+        // Step 3: ans update
+        ans = min(ans+cost[0], min(week.front().second, month.front().second));
+   
+    }
+    return ans;
+}
